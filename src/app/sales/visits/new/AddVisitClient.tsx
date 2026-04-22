a8b8afaef63bd3c0c35208609703335564e1000f
abduljamilafghan121-cell <abduljamil.afghan121@gmail.com>
2026-04-22 21:21:39 +0430

    Update import path for AppShell component

--- src/app/sales/visits/new/AddVisitClient.tsx
+++ src/app/sales/visits/new/AddVisitClient.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { AppShell } from "../../../../components/AppShell";
+import { AppShell } from "@/components/AppShell";
 import { Card } from "../../../../components/ui/Card";
 import { Button } from "../../../../components/ui/Button";
 import { Input } from "../../../../components/ui/Input";
